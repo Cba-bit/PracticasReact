0a29d3705209a8f5c3b1a3baac6a702d88868c4c
cbabit <ignaciotagle26@gmail.com>
2020-11-11 15:49:35 -0300

    Terminando mi primera app en react, contador

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const PrimeraApp = () => {
-    return <h1>Hola mundo!</h1>;
+const PrimeraApp = ({ saludo, subtitulo }) => {
+    return (
+        <>
+            <h1>{saludo}</h1>
+            <p>{subtitulo}</p>
+        </>
+    );
+};
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired,
+};
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo',
 };
 
 export default PrimeraApp;
